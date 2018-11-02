bcb419a3dd98c48188920907f1feb199c0452007
hoparkgo9ma <me@ho9.me>
2018-11-03 07:49:31 +0900

    이전 문구 업로드

---
+++ contents.js
@@ -0,0 +1,22 @@
+[
+    {
+        content: "잘했고, 잘할거야",
+        original: "2019학년도 수능특강 교재"
+    },
+    {
+        content: "이미 너는 최고야 :)",
+        original: "2019학년도 수능완성 교재"
+    },
+    {
+        content: "Prove yourself and RISE",
+        original: "RISE"
+    },
+    {
+        content: "긴장하지 말고, 지금처럼만",
+        original: ""
+    },
+    {
+        content: "",
+        original: ""
+    }
+]
