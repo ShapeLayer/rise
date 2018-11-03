90a3d05147241fd5d6d973433284ddc942f6d5fd
hoparkgo9ma <me@ho9.me>
2018-11-03 22:10:53 +0900

    변수화 / 문구 추가

--- contents.js
+++ contents.js
@@ -1,6 +1,10 @@
-[
+D100 = [ // D - 100
     {
-        content: "잘했고, 잘할거야",
+        content: "잘했고, 잘할거야.",
+        original: "2019학년도 수능특강 교재"
+    },
+    {
+        content: "잘했고, 잘할거야.",
         original: "2019학년도 수능특강 교재"
     },
     {
@@ -12,11 +16,28 @@
         original: "RISE"
     },
     {
-        content: "긴장하지 말고, 지금처럼만",
-        original: ""
-    },
+        content: "너 앉은 그 자리가 바로 희망 꽃자리",
+        original: "구상 - 꽃자리"
+    }
+]
+
+D50 = [
     {
-        content: "",
+        content: "계획대로 돼가고 있어.",
+        original: "소년점프"
+    }
+]
+
+D30 = [ // D - 30
+    {
+        content: "긴장하지 말고, 지금처럼만.",
         original: ""
+    }
+]
+
+D10 = [ // D - 10
+    {
+        content: "몇번이고 다시 일어나며 여기까지 온거야.",
+        original: "LiSA - ADAMAS"
     }
 ]
