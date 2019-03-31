9f07b82635cdf74244b6133f93b8bfd92c9d4f09
hoparkgo9ma <me@ho9.me>
2019-03-31 10:10:08 +0900

    딕셔너리 컨텐츠 선택하는 매커니즘 수정

--- assets/contents.js
+++ assets/contents.js
@@ -1,4 +1,14 @@
 contents = {
+    "common" : [
+        {
+            "content" : "잘했고, 잘할거야.",
+            "original" : "2019 수능특강 교재(2018)"
+        },
+        {
+            "content" : "꿈이 있던 장소를 찾는 여행은 시작한채로.",
+            "original" : "ASCA - RESISTER"
+        }
+    ],
     "D300" : [
         {
             "content" : "새로운 시작.",
@@ -11,14 +21,6 @@ contents = {
     ],
     "D100" : [
         {
-            "content" : "잘했고, 잘할거야.",
-            "original" : "2019 수능특강 교재(2018)"
-        },
-        {
-            "content" : "잘했고, 잘할거야.",
-            "original" : "2019 수능특강 교재(2018)"
-        },
-        {
             "content" : "이미 너는 최고야 :)",
             "original" : "2019 수능완성 교재(2018)"
         },
@@ -28,17 +30,21 @@ contents = {
         },
         {
             "content" : "너 앉은 그 자리가 바로 희망 꽃자리",
-            "original" : "구상 - 꽃자리"
+            "original" : "구상 - 꽃자리 (2018년 9월 1/2학년 모의고사)"
         },
         {
             "content" : "두꺼운 땅껍질 뚫고 나오는 아주 작은 힘",
-            "original" : "남낙현 - 봄이 오는 소리"
+            "original" : "남낙현 - 봄이 오는 소리 (2019년 3월 모의고사)"
         }
     ],
     "D50" : [
         {
             "content" : "네 실력을 보여줘. 날아올라.",
             "original" : "RISE"
+        },
+        {
+            "content" : "작은 꽃이라도 피울 수 있으면 좋겠네.",
+            "original" : "ReoNa - forget-me-not"
         }
     ],
     "D30" : [
@@ -49,6 +55,14 @@ contents = {
         {
             "content" : "두 개의 나의 발로 당당히 걸어가.",
             "original" : "WEBSTER B - 이로한"
+        },
+        {
+            "content" : "마음 단단히 먹어 알아 외롭지만 견뎌 내야 돼.",
+            "original" : "송민호 - 겁"
+        },
+        {
+            "content" : "그럼에도 저편을 향해 가.",
+            "original" : "ReoNa - forget-me-not"
         }
     ],
     "D10" : [
@@ -67,10 +81,6 @@ contents = {
         {
             "content" : "여기까지 왔어, 돌아서지 마.",
             "original" : "Imagine Dragons - Warriors"
-        },
-        {
-            "content" : "마음 단단히 먹어 알아 외롭지만 견뎌 내야 돼.",
-            "original" : "송민호 - 겁"
         }
     ]
 }
--- assets/timer.js
+++ assets/timer.js
@@ -1,8 +1,8 @@
 /* Load Target Sat */
 var sat = {
-  2018 : "Nov 15, 2018 08:40:00",
-  2019 : "Nov 14, 2019 08:40:00",
-  2020 : "Nov 19, 2020 08:40:00"
+  2018 : 'Nov 15, 2018 08:40:00',
+  2019 : 'Nov 14, 2019 08:40:00',
+  2020 : 'Nov 19, 2020 08:40:00'
 }
 
 var now = new Date().getTime();
@@ -18,19 +18,21 @@ var now_interval = new Date().getTime()
 var distance = new Date(sat[year]).getTime() - now_interval;
 var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
+var content_list = []
 if (days > 340) {
-  var content_data = contents["D300"][Math.floor(Math.random() * contents["D300"].length)];
+  content_list = content_list.concat(contents['common'], contents['D300']);
 } else if (days > 50){
-  var content_data = contents["D100"][Math.floor(Math.random() * contents["D100"].length)];
+  content_list = content_list.concat(contents['common'], contents['D100']);
 } else if (days > 30){
-  var content_data = contents["D50"][Math.floor(Math.random() * contents["D50"].length)];
+  content_list = content_list.concat(contents['common'], contents['D100'], contents['D50']);
 } else if (days > 10){
-  var content_data = contents["D30"][Math.floor(Math.random() * contents["D30"].length)];
+  content_list = content_list.concat(contents['common'], contents['D30']);
 } else {
-  var content_data = contents["D10"][Math.floor(Math.random() * contents["D10"].length)];
+  content_list = content_list.concat(contents['common'], contents['D30'], contents['D10']);
 }
-document.getElementById("sat_comment").innerHTML = content_data["content"];
-document.getElementById("sat_reference").innerHTML = content_data["original"];
+content_data = content_list[Math.floor(Math.random() * content_list.length)];
+document.getElementById('sat_comment').innerHTML = content_data['content'];
+document.getElementById('sat_reference').innerHTML = content_data['original'];
 /* Load End */
 
 /* Load Counter */
@@ -43,11 +45,11 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("sat_count").innerHTML = "다음 수능 시작까지 " + days + "일 " + hours + "시간 "
-  + minutes + "분 " + seconds + "초.";
+  document.getElementById('sat_count').innerHTML = '다음 수능 시작까지 ' + days + '일 ' + hours + '시간 '
+  + minutes + '분 ' + seconds + '초.';
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("sat_count").innerHTML = "이미 너는 최고야 :)";
+    document.getElementById('sat_count').innerHTML = '이미 너는 최고야 :)';
   }
 }, 1000);
 /* Load End */
