b5922847bda6d5111f2e959b2fef12396a90761f
hoparkgo9ma <me@ho9.me>
2019-03-31 11:51:21 +0900

    Update contents.js

--- assets/contents.js
+++ assets/contents.js
@@ -1,86 +1,102 @@
 contents = {
-    "common" : [
+    'common' : [
         {
-            "content" : "잘했고, 잘할거야.",
-            "original" : "2019 수능특강 교재(2018)"
+            'content' : '잘했고, 잘할거야.',
+            'original' : '2019 수능특강 교재(2018)'
         },
         {
-            "content" : "꿈이 있던 장소를 찾는 여행은 시작한채로.",
-            "original" : "ASCA - RESISTER"
+            'content' : '꿈이 있던 장소를 찾는 여행은 시작한채로.',
+            'original' : 'ASCA - RESISTER'
+        },
+        {
+            'content' : '그대 오늘도 잘해냈어요',
+            'original' : '최한솔 - 그대 오늘도 잘해냈어요'
         }
     ],
-    "D300" : [
+    'D300' : [
         {
-            "content" : "새로운 시작.",
-            "original" : "Windows XP K/KN"
+            'content' : '새로운 시작.',
+            'original' : 'Windows XP K/KN'
         },
         {
-            "content" : "바라봐 어제와 다른 나의 오늘.",
-            "original" : "WEBSTER B - 이로한"
+            'content' : '바라봐 어제와 다른 나의 오늘.',
+            'original' : 'WEBSTER B - 이로한'
         }
     ],
-    "D100" : [
+    'D100' : [
         {
-            "content" : "이미 너는 최고야 :)",
-            "original" : "2019 수능완성 교재(2018)"
+            'content' : '이미 너는 최고야 :)',
+            'original' : '2019 수능완성 교재(2018)'
         },
         {
-            "content" : "꿈꾸는 대로 이루어질 거야",
-            "original" : "2019 메가스터디 1학기 플래너"
+            'content' : '꿈꾸는 대로 이루어질 거야',
+            'original' : '2019 메가스터디 1학기 플래너'
         },
         {
-            "content" : "너 앉은 그 자리가 바로 희망 꽃자리",
-            "original" : "구상 - 꽃자리 (2018년 9월 1/2학년 모의고사)"
+            'content' : '너 앉은 그 자리가 바로 희망 꽃자리',
+            'original' : '구상 - 꽃자리 (2018년 9월 1/2학년 모의고사)'
         },
         {
-            "content" : "두꺼운 땅껍질 뚫고 나오는 아주 작은 힘",
-            "original" : "남낙현 - 봄이 오는 소리 (2019년 3월 모의고사)"
+            'content' : '두꺼운 땅껍질 뚫고 나오는 아주 작은 힘',
+            'original' : '남낙현 - 봄이 오는 소리 (2019년 3월 모의고사)'
         }
     ],
-    "D50" : [
+    'D50' : [
         {
-            "content" : "네 실력을 보여줘. 날아올라.",
-            "original" : "RISE"
+            'content' : '네 실력을 보여줘. 날아올라.',
+            'original' : 'RISE'
         },
         {
-            "content" : "작은 꽃이라도 피울 수 있으면 좋겠네.",
-            "original" : "ReoNa - forget-me-not"
+            'content' : '작은 꽃이라도 피울 수 있으면 좋겠네.',
+            'original' : 'ReoNa - forget-me-not'
+        },
+        {
+            'content' : '힘을내 여기까지 왔잖아',
+            'original' : '박효신 - 숨'
         }
     ],
-    "D30" : [
+    'D30' : [
         {
-            "content" : "긴장하지 말고, 지금처럼만.",
-            "original" : ""
+            'content' : '긴장하지 말고, 지금처럼만.',
+            'original' : ''
         },
         {
-            "content" : "두 개의 나의 발로 당당히 걸어가.",
-            "original" : "WEBSTER B - 이로한"
+            'content' : '두 개의 나의 발로 당당히 걸어가.',
+            'original' : 'WEBSTER B - 이로한'
         },
         {
-            "content" : "마음 단단히 먹어 알아 외롭지만 견뎌 내야 돼.",
-            "original" : "송민호 - 겁"
+            'content' : '마음 단단히 먹어 알아 외롭지만 견뎌 내야 돼.',
+            'original' : '송민호 - 겁'
         },
         {
-            "content" : "그럼에도 저편을 향해 가.",
-            "original" : "ReoNa - forget-me-not"
+            'content' : '그럼에도 저편을 향해 가.',
+            'original' : 'ReoNa - forget-me-not'
+        },
+        {
+            'content' : '전부 괜찮을거야, 괜찮을거야',
+            'original' : '로코베리 - 괜찮아, 청춘'
         }
     ],
-    "D10" : [
+    'D10' : [
         {
-            "content" : "몇번이고 다시 일어나며 여기까지 온거야.",
-            "original" : "LiSA - ADAMAS"
+            'content' : '몇번이고 다시 일어나며 여기까지 온거야.',
+            'original' : 'LiSA - ADAMAS'
         },
         {
-            "content" : "지금이야, 하늘로 비상할 시간.",
-            "original" : "RISE"
+            'content' : '지금이야, 하늘로 비상할 시간.',
+            'original' : 'RISE'
         },
         {
-            "content" : "그 동안 잘했어.",
-            "original" : "2019 메가스터디 1학기 플래너"
+            'content' : '그 동안 잘했어.',
+            'original' : '2019 메가스터디 1학기 플래너'
         },
         {
-            "content" : "여기까지 왔어, 돌아서지 마.",
-            "original" : "Imagine Dragons - Warriors"
+            'content' : '여기까지 왔어, 돌아서지 마.',
+            'original' : 'Imagine Dragons - Warriors'
+        },
+        {
+            'content' : '난 행복한 사람 참 수고 많이 했어.',
+            'original' : '윤종신 - 나에게 하는 격려'
         }
     ]
 }
